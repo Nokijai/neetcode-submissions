c3222afc9a09a1d6c3942daeb40742276639d530
Noki <140824702+Nokijai@users.noreply.github.com>
2026-09-15 23:55:56 +0800

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> encode_mapper;
+        vector<vector<string>> ans;
+        for(const auto& str: strs){
+            int count[26] = {0};
+            for(char c : str){
+                count[c - 'a']++;
+            }
+
+            string key = "";
+            for(int num: count){
+                key += '#' + to_string(num);
+            }
+
+            encode_mapper[key].push_back(str);
+        }
+
+        for(auto &[key , value]: encode_mapper){
+            ans.push_back(move(value));
+        }
+
+        return ans;
+
+
+    }
+};
