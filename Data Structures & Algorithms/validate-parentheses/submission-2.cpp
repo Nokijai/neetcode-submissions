1f05f37a0d412a655c544db1eba7ae64bce118e8
Noki <140824702+Nokijai@users.noreply.github.com>
2026-09-16 22:59:43 +0800

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        if(s.size() == 1) return false;
+
+        for(const auto& c: s){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }else{
+                if(st.empty()){
+                    return false;
+                }else{
+                    if((c == ')' && st.top() == '(') || 
+                        (c == '}' && st.top() == '{') ||
+                        (c == ']' && st.top() == '[')
+                    ){
+                        st.pop();
+                    }else{
+                        st.push(c);
+                    }
+                }
+            }
+        }
+
+        if(st.size() != 0){
+            return false;
+        }
+        return true;
+    }
+};
